ea9b70a3d9c2b7801098b1f3db1e015c70042ab1
starer1333 <wangdacongming@icloud.com>
2026-09-22 05:41:15 -0700

    Use Playwright managed Chromium in E2E

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,6 @@ module.exports = defineConfig({
   reporter: 'line',
   use: {
     baseURL: process.env.ATLAS_E2E_BASE_URL || 'http://127.0.0.1:8766',
-    channel: 'chrome',
     headless: true,
     viewport: { width: 1440, height: 1000 },
     trace: 'retain-on-failure'
